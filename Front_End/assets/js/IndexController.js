9d66ed4e839189f545def43488078ada690ad6ab
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-06-25 02:06:29 +0530

    navigation

--- Front_End/assets/js/IndexController.js
+++ Front_End/assets/js/IndexController.js
@@ -5,14 +5,22 @@ $("#get").click(function () {
     $("#signUp").css('display','none');
 });
 
-$("#h").click(function () {
-    $("#userLogin").css('display','none');
-    $("#home").css('display','block');
-    $("#bookContent").css('display','none');
+$("#home").click(function () {
+    $("#login").css('display','none');
+    $("#hero").css('display','block');
+    $("#table").css('display','none');
+    $("#signUp").css('display','none');
 });
 
 $("#btnLogin").click(function () {
-    $("#bookContent").css('display','block');
-    $("#home").css('display','none');
-    $("#userLogin").css('display','none');
+    $("#login").css('display','none');
+    $("#hero").css('display','none');
+    $("#table").css('display','block');
+    $("#signUp").css('display','none');
+});
+$("#btnSignUp").click(function () {
+    $("#login").css('display','block');
+    $("#hero").css('display','none');
+    $("#table").css('display','none');
+    $("#signUp").css('display','none');
 });
